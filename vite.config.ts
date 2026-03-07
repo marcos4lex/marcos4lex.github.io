42a43765de2860ceba22268e24463553df7478b2
marcos4lex <marcos4lex@users.noreply.github.com>
2026-03-07 15:03:52 -0300

    atualização pós quebra

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Se o domínio customizado está corretamente configurado, mantenha '/'. Caso contrário, ajuste para '/marcos4lex-portfolio/'
 })
